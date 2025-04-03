00b199028b742b36a0adaca8b091a9390dbf8022
231054 <231054@CPD03-M12>
2025-04-03 15:34:05 -0300

    tcc

---
+++ meu_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+
+
+ main() {
+  runApp(
+    Widget build(BuildContext context){
+      return Scafford(
+    
+      child: Container(
+      decoration: BoxDecoration(image:DecorationImage(image:AssetImage("assets/image/fundo1.jpeg"),),) ,
+      child:(Text("data", textDirection: TextDirection.ltr,)
+       
+      )),
+    )
+
+
+ 
+)
+);
+ //runapp
+  }    
+}
