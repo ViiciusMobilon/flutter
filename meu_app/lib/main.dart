59bfad1aea702e67f1b4fe3e5847f1291e6aee6d
231054 <231054@CPD03-M12>
2025-04-07 15:55:42 -0300

    começo do curso

--- meu_app/lib/main.dart
+++ meu_app/lib/main.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
 
+void main () => runApp(MyApp());
 
-
-
- main() {
-  runApp(
-    Widget build(BuildContext context){
-      return Scafford(
+class MyApp extends StatelessWidget{
+    @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'FLUTTER DEMO',
+        theme: ThemeData(
+            primarySwatch: Colors.blue
+        ),
+        home:Scaffold(
+          body: HomeWidget(),
+        ),
+    );
     
+  }
+}
+ class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //container tem tudo dentro dele
       child: Container(
-      decoration: BoxDecoration(image:DecorationImage(image:AssetImage("assets/image/fundo1.jpeg"),),) ,
-      child:(Text("data", textDirection: TextDirection.ltr,)
-       
-      )),
-    )
-
-
- 
-)
-);
- //runapp
-  }    
+        width: 300,
+        height: 300,
+        color: Colors.blue,
+        child: Container(
+          width: 50,// filhos erdão o tamanho dos pais
+          height: 50,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
 }
--- meu_app/test/widget_test.dart
+++ meu_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:meu_app/main.dart';
+import 'package:meu_app/main';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
