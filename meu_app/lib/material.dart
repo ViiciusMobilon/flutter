a2c18cad3bc1330839ce6d89cfeb0a824e76ab71
231054 <231054@CPD03-M12>
2025-05-22 15:52:54 -0300

    textfield

--- meu_app/lib/material.dart
+++ meu_app/lib/material.dart
@@ -16,8 +16,29 @@ class Materialwidget extends StatelessWidget {
       ( appBar: AppBar(
         title: Text('Exemplo FAB'),
       ),
+
       body: Center(
-        child: Text('Conteúdo da tela'),
+        child: Container(
+          height: 300,
+         width: 200,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 30,),
+                  MyWidget(),
+                  SizedBox(height: 30,),
+                  senha(),
+                
+               
+                Container(
+                  
+                  child: Text('Conteúdo da tela'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _onFabPressed ,
@@ -33,3 +54,76 @@ class Materialwidget extends StatelessWidget {
     );
   }
 }
+
+class senha extends StatefulWidget {
+  const senha({super.key});
+
+  @override
+  _senhaState createState() =>_senhaState();
+}
+
+class _senhaState extends State<senha> {
+  @override
+  bool senha = true;
+
+  void mudarvisao(){
+    setState(() {
+      senha=! senha;
+    });
+  }
+  Widget build(BuildContext context) {
+    return 
+          TextField(
+                   
+                   maxLength: 5,  
+                   autofocus: false,
+                   obscureText: senha,
+             decoration: InputDecoration(
+            labelText:"senha" ,labelStyle: TextStyle(color: Colors.black)  ,
+            hintText: "123456",
+            border: UnderlineInputBorder(
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: const Color.fromARGB(255, 255, 0, 0), width: 1.5),
+              
+            ),
+            disabledBorder:UnderlineInputBorder(
+              borderSide: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+            ),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+            ),
+            suffixIcon: IconButton( icon:Icon(senha ? Icons.visibility_off :Icons.visibility ), onPressed: mudarvisao )
+             ),
+               
+        
+         );
+  }
+}
+ 
+ class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+
+      decoration: InputDecoration(
+        labelText:"email" ,labelStyle: TextStyle(color: Colors.black)  ,
+            hintText: "blabla@gmail.com",
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.red)
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.black)
+        )
+      ),
+    );
+  }
+}
+
