a914c5279f300fd51cb8e5c1ee3d8bd9e0810798
231054 <231054@CPD03-M12>
2025-05-26 15:37:37 -0300

    cadastro fim

--- meu_app/lib/cadastro.dart
+++ meu_app/lib/cadastro.dart
@@ -2,25 +2,73 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Cadastro());
 class Cadastro extends StatelessWidget {
-  const Cadastro({super.key});
+   Cadastro({super.key});
 
+final Color cor =  Color.fromRGBO(15, 40, 89, 1.0);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(),
+      
       home: Scaffold(
-        
+        appBar: AppBar(
+          title:Text("Cadastro",
+        style: TextStyle( color: Colors.white),
+        textAlign: TextAlign.center,
+          ),
+          centerTitle: true,
+                    backgroundColor: cor,
+        ),
         body: ListView(
 children: [
-     Padding(padding: EdgeInsets.only(top:100, left: 100,right: 100),
+
+    Padding(padding: EdgeInsets.only()),
+    Container(
+    width: MediaQuery.of(context).size.width * 0.4, // 70% da largura da tela
+    height: MediaQuery.of(context).size.height * 0.2, // 30% da altura da tela
+    decoration: BoxDecoration(
+    image: DecorationImage(
+      image: AssetImage("assets/image/LOGO.png"),
+      fit: BoxFit.contain))),
+
+      Padding(padding: EdgeInsets.only(bottom:10,left:MediaQuery.of(context).size.width*0.2 ,right: MediaQuery.of(context).size.width*0.1,top: 40)),
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+       children: [
+       
+        Text("ja tem uma conta? clique "),
+        
+        GestureDetector(
+          onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context)=> Cadastro() )),
+          child: Text("Aqui", style: TextStyle(color: Colors.blueAccent),),
+        )
+       ],
+      ),
+
+     Padding(padding: EdgeInsets.only(top:MediaQuery.of(context).size.width*0.1, 
+     left: MediaQuery.of(context).size.width*0.2, 
+     bottom:MediaQuery.of(context).size.width*0.1,
+     right:MediaQuery.of(context).size.width*0.2  ),
      child: email(),),
-     Padding(padding: EdgeInsets.only())
+
+     Padding(padding: EdgeInsets.only(top:MediaQuery.of(context).size.width*0.1, 
+     left: MediaQuery.of(context).size.width*0.2, 
+     bottom:MediaQuery.of(context).size.width*0.2,
+     right:MediaQuery.of(context).size.width*0.2  ),
+     child: senha(),),
+
+
+     
+
+
 ],
 
         )
 
           
-        )
+        ),
+        debugShowCheckedModeBanner: false,
         );
   }
 }
@@ -47,6 +95,78 @@ class _emailState extends State<email> {
         )
       ),
 
+    );
+  }
+}
+
+class senha extends StatefulWidget {
+  const senha({super.key});
+
+  @override
+  _senhaState createState() =>_senhaState();
+}
+
+class _senhaState extends State<senha> {
+  @override
+  bool senha = true;
+
+  void mudarvisao(){
+    setState(() {
+      senha=! senha;
+    });
+  }
+  Widget build(BuildContext context) {
+    return 
+          TextField(
+                   
+                   maxLength: 5,  
+                   autofocus: false,
+                   obscureText: senha,
+             decoration: InputDecoration(
+            labelText:"senha" ,labelStyle: TextStyle(color: Colors.black)  ,
+            hintText: "123456",
+            border: UnderlineInputBorder(
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: const Color.fromARGB(255, 255, 0, 0), width: 1.5),
+              
+            ),
+            disabledBorder:UnderlineInputBorder(
+              borderSide: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+            ),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: const Color.fromARGB(255, 0, 0, 0)),
+            ),
+            suffixIcon: IconButton( icon:Icon(senha ? Icons.visibility_off :Icons.visibility ), onPressed: mudarvisao )
+             ),
+               
+        
+         );
+  }
+}
+ 
+ class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+
+      decoration: InputDecoration(
+        labelText:"email" ,labelStyle: TextStyle(color: Colors.black)  ,
+            hintText: "blabla@gmail.com",
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.red)
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.black)
+        )
+      ),
     );
   }
 }
