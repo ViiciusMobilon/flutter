b42600eb7f3d8a9037425694e7ef2ffcd7e6ac06
231054 <231054@CPD03-M12>
2025-08-07 15:14:31 -0300

    fim do dropdowl

--- projeto_principal/lib/Empresa.dart
+++ projeto_principal/lib/Empresa.dart
@@ -4,6 +4,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:projeto_principal/CEP.dart';
 import 'package:projeto_principal/Escolha.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 
 final maskFormatter = MaskTextInputFormatter(
   mask: '(##) #####-####',
@@ -90,7 +91,7 @@ class Empresa extends StatelessWidget {
                 bottom: MediaQuery.of(context).size.width * 0.1,
                 right: MediaQuery.of(context).size.width * 0.1,
               ),
-              child: area(),
+              child: Area(),
             ),
          
            Padding(
@@ -312,39 +313,89 @@ class _cpfState extends State<cpf> {
     );
   }
 }
- class area extends StatefulWidget {
-  const area({super.key});
+ class Area extends StatelessWidget {
+  final dropValue = ValueNotifier('');
+  final dropOpcoes = [
+    'Pedreiro',
+    'Pintor',
+    'Eletricista',
+    'Encanador',
+    'Marceneiro',
+    'Jardineiro',
+    'Gesseiro',
+    'Serralheiro',
+    'Vidraceiro',
+    'Alvenaria',
+    'Telhadista',
+    'Azulejista',
+    'Instalador de drywall',
+    'Servente de obras',
+    'Outros'
+  ];
 
-  @override
-  State<area> createState() => _areaState();
-}
+  Area({super.key});
 
-class _areaState extends State<area> {
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        labelText: "Area",
-        labelStyle: TextStyle(
-          color: Colors.black,
-          fontSize: MediaQuery.of(context).size.width * 0.05, fontFamily: "Poppins",
-        ),
-        hintText: "Pedreiro",
-        hintStyle: TextStyle(
-          fontSize: MediaQuery.of(context).size.width * 0.05, fontFamily: "Poppins",
-        ),
-        focusedBorder:OutlineInputBorder(
-           borderRadius: BorderRadius.circular(20),
-          borderSide: BorderSide(
-            color: const Color.fromRGBO(121, 180, 217, 1),
-          ),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(
-            color: Colors.grey,
-          ),
-        ),
+    return Center(
+      child: ValueListenableBuilder(
+        valueListenable: dropValue,
+        builder: (BuildContext context, String value, _) {
+          return SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: DropdownSearch<String>(
+              items: dropOpcoes,
+              selectedItem: value.isEmpty ? null : value,
+              onChanged: (String? newValue) {
+                dropValue.value = newValue ?? '';
+              },
+              popupProps: PopupProps.menu(
+                showSearchBox: true,
+                searchFieldProps: TextFieldProps(
+                  decoration: InputDecoration(
+                    labelText: "Pesquisar área...",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                fit: FlexFit.loose,
+                constraints: BoxConstraints(
+                  maxHeight: 250,
+                ),
+              ),
+              dropdownDecoratorProps: DropDownDecoratorProps(
+                dropdownSearchDecoration: InputDecoration(
+                  labelText: "Área de atuação",
+                  hintText: "Escolha a área de atuação",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color.fromRGBO(121, 180, 217, 1),
+                      width: 1.5,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              dropdownBuilder: (context, selectedItem) {
+                return Text(
+                  selectedItem ?? "Escolha a área de atuação",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.045,
+                    fontFamily: "Poppins",
+                  ),
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
--- projeto_principal/lib/Prestador.dart
+++ projeto_principal/lib/Prestador.dart
@@ -4,6 +4,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:projeto_principal/CEP.dart';
 import 'package:projeto_principal/Escolha.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 
 final maskFormatter = MaskTextInputFormatter(
   mask: '(##) #####-####',
@@ -90,7 +91,7 @@ class Prestador extends StatelessWidget {
                 bottom: MediaQuery.of(context).size.width * 0.1,
                 right: MediaQuery.of(context).size.width * 0.1,
               ),
-              child: area(),
+              child: Area(),
             ),
          
            Padding(
@@ -313,47 +314,96 @@ class _cpfState extends State<cpf> {
   }
 }
 
-class area extends StatefulWidget {
-  const area({super.key});
 
-  @override
-  State<area> createState() => _areaState();
-}
 
-class _areaState extends State<area> {
-   String? selectedOption = 'Maçã'; // valor inicial selecionado
-  List<String> options = ['Maçã', 'Banana', 'Laranja', 'Uva'];
+class Area extends StatelessWidget {
+  final dropValue = ValueNotifier('');
+  final dropOpcoes = [
+    'Pedreiro',
+    'Pintor',
+    'Eletricista',
+    'Encanador',
+    'Marceneiro',
+    'Jardineiro',
+    'Gesseiro',
+    'Serralheiro',
+    'Vidraceiro',
+    'Alvenaria',
+    'Telhadista',
+    'Azulejista',
+    'Instalador de drywall',
+    'Servente de obras',
+    'Outros'
+  ];
+
+  Area({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DropdownButton<String>(
-              value: selectedOption,
-              items: options.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
+      child: ValueListenableBuilder(
+        valueListenable: dropValue,
+        builder: (BuildContext context, String value, _) {
+          return SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: DropdownSearch<String>(
+              items: dropOpcoes,
+              selectedItem: value.isEmpty ? null : value,
               onChanged: (String? newValue) {
-                setState(() {
-                  selectedOption = newValue;
-                });
+                dropValue.value = newValue ?? '';
+              },
+              popupProps: PopupProps.menu(
+                showSearchBox: true,
+                searchFieldProps: TextFieldProps(
+                  decoration: InputDecoration(
+                    labelText: "Pesquisar área...",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                fit: FlexFit.loose,
+                constraints: BoxConstraints(
+                  maxHeight: 250,
+                ),
+              ),
+              dropdownDecoratorProps: DropDownDecoratorProps(
+                dropdownSearchDecoration: InputDecoration(
+                  labelText: "Área de atuação",
+                  hintText: "Escolha a área de atuação",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: const Color.fromRGBO(121, 180, 217, 1),
+                      width: 1.5,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              dropdownBuilder: (context, selectedItem) {
+                return Text(
+                  selectedItem ?? "Escolha a área de atuação",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.045,
+                    fontFamily: "Poppins",
+                  ),
+                );
               },
             ),
-            const SizedBox(height: 20),
-            Text(
-              'Selecionado: $selectedOption',
-              style: const TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
-      );
-    
+          );
+        },
+      ),
+    );
   }
 }
+
 
 class botao extends StatefulWidget {
   const botao({super.key});
